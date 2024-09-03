f088c286422778a41d297b6617e2059f4c77ee32
Matt MacLeod <mattmacleod12@gmail.com>
2024-09-03 14:24:51 -0700

    working internet server

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-
 from smartAssistant import SmartAssistant  # Import the custom class
 from flask import Flask, request, jsonify, render_template
+from flask_basicauth import BasicAuth
+import os 
+
+
 
 app = Flask(__name__)
+
+# Configure basic authentication
+app.config['BASIC_AUTH_USERNAME'] = os.getenv('JARVIS_SERVER_USER')  # Change this to a strong username
+app.config['BASIC_AUTH_PASSWORD'] = os.getenv('JARVIS_SERVER_PASSWORD')  # Change this to a strong password
+
+basic_auth = BasicAuth(app)
 
 # Initialize the SmartAssistant class
 assistant = SmartAssistant("JARVIS")
 
 @app.route('/', methods=['GET'])
+@basic_auth.required
 def index():
     return render_template('index.html')
 
 @app.route('/get_response', methods=['POST'])
+@basic_auth.required
 def get_response():
     if request.content_type != 'application/json':
         return jsonify({'error': 'Content-Type must be application/json'}), 400
@@ -25,4 +36,4 @@ def get_response():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
